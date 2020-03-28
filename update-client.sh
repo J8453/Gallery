c1420ac2dd1fedd0b54f2ca4f59ba389d9e95ca2
lmnooney <eunice.jn.tsai@gmail.com>
2020-03-28 10:39:07 +0800

    rm .sh

--- update-client.sh
+++
@@ -1,7 +0,0 @@
-fuser -k 5000/tcp
-cd frontend/
-pwd
-git pull
-npm install
-npm run build
-screen -d -m -L serve -s build
--- update-server.sh
+++
@@ -1,6 +0,0 @@
-fuser -k 3006/tcp
-cd backend/
-pwd
-git pull
-npm install
-screen -d -m -L npm run start
